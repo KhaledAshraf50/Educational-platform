ae438c175591d0f4dc283d0bb15dcde4809f427c
Ahmed Morsi <morsi6540@gmail.com>
2025-11-06 23:04:41 +0200

    Initial commit

---
+++ Controllers/homepage/HomepageController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Luno_platform.Controllers.homepage
+{
+  
+    public class HomepageController : Controller
+    {
+        public IActionResult mainpage()
+        {
+            return View();
+        }
+
+
+
+        public IActionResult login_in()
+        {
+            return View();
+        }
+
+
+        public IActionResult sign_up()
+        {
+            return View();
+        }
+
+        public IActionResult show_all_teacher()
+        {
+            return View();
+        }
+
+        public IActionResult show_portfolio_teacher()
+        {
+            return View();
+        }
+
+        public IActionResult show_courses_teacher()
+        {
+            return View();
+        }
+
+
+        public IActionResult show_details_courses()
+        {
+            return View();
+        }
+
+    }
+
+
+}
