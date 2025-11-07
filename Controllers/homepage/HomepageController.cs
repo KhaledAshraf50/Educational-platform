73b4eeb2dc44fa028f10f09b24584a7d38940f6b
Morsi00 <morsi6540@gmail.com>
2025-11-07 14:22:14 +0200

    EX

--- Controllers/homepage/HomepageController.cs
+++ Controllers/homepage/HomepageController.cs
@@ -5,7 +5,7 @@ namespace Luno_platform.Controllers.homepage
   
     public class HomepageController : Controller
     {
-        public IActionResult mainpage()
+        public IActionResult ainpage()
         {
             return View();
         }
