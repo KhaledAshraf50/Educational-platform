c2befac44f3b5dfb4d4cfd1311e5930f8bce97b4
Khaled Ashraf <158307258+KhaledAshraf50@users.noreply.github.com>
2025-11-30 19:17:29 +0200

    Add Progress In Student Page

--- Viewmodel/mainPage_Student_ViewModel.cs
+++ Viewmodel/mainPage_Student_ViewModel.cs
@@ -6,7 +6,11 @@ namespace Luno_platform.Viewmodel
     public class mainPage_Student_ViewModel
     {
         public Student? Student { get;  set; }
-        public List<Courses> Courses { get;  set; } 
+        public List<Courses> Courses { get;  set; }
+        public double OverallProgress { get; set; }
+        public double ExamProgress { get; set; }
+        public double TaskProgress { get; set; }
+
 
     }
 }
